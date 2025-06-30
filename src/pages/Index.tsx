0ef6e8a8716770724f560d270a07268cb3821660
Elia Zavatta <zavattaelia@gmailn.com>
2025-06-30 16:25:37 +0200

    aggiunto app-ads.txt

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -4,7 +4,6 @@ import Header from "@/components/Header";
 import HeroSection from "@/components/HeroSection";
 import ProjectsSection from "@/components/ProjectsSection";
 import AboutSection from "@/components/AboutSection";
-import ContactSection from "@/components/ContactSection";
 import Footer from "@/components/Footer";
 import CustomCursor from "@/components/CustomCursor";
 
